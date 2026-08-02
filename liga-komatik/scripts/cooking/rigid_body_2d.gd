0e380963cb6c2cf93ee8e682629d3c7d3bbba03e
tastifulback <tastifulback@gmail.com>
2026-08-02 21:36:23 +0700

    MORE COOKING

--- liga-komatik/scripts/cooking/rigid_body_2d.gd
+++ liga-komatik/scripts/cooking/rigid_body_2d.gd
@@ -1,27 +1,46 @@
 extends RigidBody2D
 class_name food
-
-@export var gravity_strength: float = 800.0
-
+@onready var time = $Timer
+@export var gravity_strength: float = 400.0
+@onready var spatula  = $"../Spatula"
+@onready var wajan = $"../wajan"
+const cookTime = 10
+signal done(nutrition)
+@export var nutrition = 0.5
+@export var timePass = 0
 func _ready() -> void:
 	# Disable the default global Y-down gravity on this body
 	gravity_scale = 0.0
-	var wajan = $"../wajan"
+	
 	wajan.body_exited.connect(dirty)
+	wajan.body_entered.connect(mulai)
+	
 func dirty(junk)-> void:
 	if junk == self:
 		queue_free()
 
 func _physics_process(delta: float) -> void:
-	# Calculate the center point of the current viewport
+	
 	var center_of_screen: Vector2 = get_viewport_rect().size / 2.0
-	
-	
 	var direction_to_center: Vector2 = global_position.direction_to(center_of_screen)
-	
-
 	apply_central_force(direction_to_center * gravity_strength)
 
+func _on_timer_timeout() -> void:
+	if timePass > cookTime:
+		queue_free()
+		done.emit(nutrition)
+	else: 
+		timePass += 1
+		print("continue")
+	
+func mulai(junk):
+	if junk == self: 
+		time.start()
+		print("start")
 
-func _on_wajan_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+func _on_body_entered(body: Node) -> void:
+	
+	if body == spatula:
+		timePass += 1
+		print("kena")
+	
