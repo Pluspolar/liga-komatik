2be2d8c5fbb6110fd1ab9e3234cdc5d05aaee00b
tastifulback <tastifulback@gmail.com>
2026-08-10 17:36:12 +0700

    Pelanggan started

--- liga-komatik/scenes/Pelanggang/desciion.gd
+++ liga-komatik/scenes/Pelanggang/desciion.gd
@@ -1,11 +1,14 @@
 extends CanvasLayer
 @onready var dialog = $dialogue
 const cook = preload("uid://cnqh7ocygiu32")
+
+var list = ["Aku laparrrr banget, keluargaku dibunuh, cuma ini yang kupunya", "Uhhhh, aku lapar", "Aku mau porsi Jumbo"]
+var times = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	dialog.add_text(list[0],15,"Random Woman")
 	
-	dialog.visible = false
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,11 +18,15 @@ func _process(delta: float) -> void:
 func _on_cooook_button_down() -> void:
 	set_process(false)
 	get_tree().change_scene_to_packed(cook)
-
+	times =0
 
 func _on_repeat_button_down() -> void:
-	pass # Replace with function body.
-
+	if times < list.size():
+		dialog.add_text(list[times],15,"Random Woman")
+		times += 1
+	
+	
+	
 
 func _on_gossip_button_down() -> void:
-	pass # Replace with function body.
+	dialog.add_text("di sigma ada skibidi, kalo skibid ke sigma",15,"Random Woman")
