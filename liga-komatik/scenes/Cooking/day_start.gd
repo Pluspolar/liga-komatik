ddc5dda3142a92d91e732bd01e8cdc99afe9ed05
tastifulback <tastifulback@gmail.com>
2026-08-17 19:03:28 +0700

    more text

--- liga-komatik/scenes/Cooking/day_start.gd
+++ liga-komatik/scenes/Cooking/day_start.gd
@@ -3,21 +3,15 @@ extends Node2D
 var radio_text : Array = [
 "Tentara-tentara mulai melandas di daerah IKN, pesawat AU mulai melewati langit IKN",
 "Sekolah 67 kena bomb di hari ini. 420 dari 1260 siswa-siswi ditemukan wafat.",
-"Negara musuh mengebom pipa air, IKN tidak memiliki sumber air yang lain, Pemerintah mengirim truk air keluar dalam IKN",
-"Pemerintah menyatakan Tentara musuh mulai melandas di pesisir-pesisir kalimantan, jendral Sutarjo menyatan bahwa IKN terlindungi",
-"Jaringan listrik dan telekomunikasi di pusat IKN lumpuh total akibat serangan rudal yang menargetkan Gardu Induk Sepaku.",
-
-"Jembatan Pulau Balang dihantam bom jelajah; jalur evakuasi utama darat menuju Balikpapan resmi terputus.",
-
-"RSU Sepaku menyatakan situasi krisis massal, lebih dari 800 korban luka-luka memadati lorong rumah sakit di tengah keterbatasan pasokan medis.",
-
-"Sistem pertahanan udara TNI berhasil menembak jatuh dua pesawat tempur musuh yang mencoba menembus zona udara Istana Garuda.",
-
-"Presiden mengumumkan pengalihan pusat komando darurat ke lokasi rahasia setelah Kompleks Kementerian Hankam mengalami kerusakan struktur akibat artileri.",
-
-"Batalyon Marinir dikerahkan ke muara Sungai Mahakam untuk membendung penetrasi armada amfibi musuh yang mencoba masuk dari arah pantai.",
-
-"Pasukan gabungan TNI dan sukarelawan lokal mendirikan garis pertahanan terakhir di Km 38 Tol Balikpapan-Samarinda."
+"Negara musuh mengebom pipa-pipa air IKN, kita tidak memiliki sumber air yang lain.",
+"Kami menyatakan Tentara musuh mulai melandas di pesisir kalimantan, jendral Sutarjo menyatan bahwa IKN terlindungi",
+"Jaringan listrik di pusat IKN lumpuh total akibat rudal yang Mengenai Trafo Induk.",
+"Jembatan Pulau Skibidi dihantam bom. Jalur evakuasi utama darat menuju IKN resmi terputus.",
+"Jendral Sutarjo menyatakan situasi krisis massal, lebih dari 800 korban luka-luka memadati lorong rumah sakit.",
+"Sistem pertahanan udara TNI berhasil menembak jatuh dua pesawat tempur musuh yang mencoba menembus zona udara IKN.",
+"Presiden mengumumkan pengalihan pusat komando darurat ke lokasi rahasia setelah Kompleks Kementerian Sigma kena rudal.",
+"Batalyon Marinir dikirim untuk membendung penetrasi armada amfibi musuh yang mencoba masuk dari arah pantai.",
+"Pasukan gabungan TNI dan sukarelawan lokal mendirikan garis pertahanan terakhir di Km 38 Tol Charlie-Kirk."
 ]
 
 func _ready() -> void:
